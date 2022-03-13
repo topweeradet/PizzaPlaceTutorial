6765996e39ce4a3ecf8901a4a4aeaeb6a2151222
topweeradet <43951704+topweeradet@users.noreply.github.com>
2022-03-13 15:21:58 +0700

    Implement Dependency injection with Services

--- PizzaPlace/Client/Program.cs
+++ PizzaPlace/Client/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PizzaPlace.Client;
+using PizzaPlace.Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddTransient<IMenuService, HardCodedMenuService>();
+builder.Services.AddTransient<IOrderService, ConsoleOrderService>();
 
 await builder.Build().RunAsync();
---
+++ PizzaPlace/Shared/ConsoleOrderService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPlace.Shared
+{
+    public class ConsoleOrderService : IOrderService
+    {
+        public ValueTask PlaceOrder(ShoppingBasket shoppingBasket)
+        {
+            Console.WriteLine($"Placing order for {shoppingBasket.Customer.Name}");
+            return new ValueTask();
+        }
+    }
+}
