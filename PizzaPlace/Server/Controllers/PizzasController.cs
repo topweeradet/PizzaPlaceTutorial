f806af8bb9291224c953db2c8828b1bc49336b15
topweeradet <43951704+topweeradet@users.noreply.github.com>
2022-03-14 14:34:06 +0700

    Add 1st API, PizzaController,   to Server

---
+++ PizzaPlace/Server/Controllers/PizzasController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PizzaPlace.Shared;
+
+namespace PizzaPlace.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        private static readonly List<Pizza> pizzas = new List<Pizza>
+        {
+            new Pizza(1, "Pepperoni", 8.99M, Spiciness.Spicy),
+            new Pizza(2, "Margarita", 7.99M, Spiciness.None),
+            new Pizza(3, "Diabolo", 9.99M, Spiciness.Hot)
+        };
+
+        [HttpGet("/pizzas")]
+        public IQueryable<Pizza> GetPizzas() => pizzas.AsQueryable();
+    }
+}
